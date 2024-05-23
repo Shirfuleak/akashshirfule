a05593fcfefc01811c2bd7c5b298247dbc6ce001
Shirfuleak <shirfuleakash8@gmaoil.com>
2024-05-24 03:23:58 +0530

    added changes in works page

--- src/components/Works/Work.js
+++ src/components/Works/Work.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './work.css';
-import portfolio1 from '../../assets/portfolio1.jpg'
-import portfolio2 from '../../assets/porfolio2.jpg'
-import portfolio3 from '../../assets/porfolio3.jpg'
-import portfolio4 from '../../assets/porfolio4.jpg'
-import portfolio5 from '../../assets/porfolio5.webp'
-import portfolio6 from '../../assets/porfolio6.png'
+import portfolio1 from '../../assets/portfolio1.png'
+import portfolio2 from '../../assets/portfolio2.png'
+import portfolio3 from '../../assets/portfolio3.png'
+import portfolio4 from '../../assets/portfolio4.png'
+import portfolio5 from '../../assets/portfolio5.png'
+import portfolio6 from '../../assets/portfolio6.png'
 
 const Work = () => {
   return (
@@ -20,7 +20,7 @@ const Work = () => {
         <img className='worksImg' alt='' src={portfolio5}/>
         <img className='worksImg' alt='' src={portfolio6}/>
     </div>
-    <button className='workBtn'>See More</button>
+    {/* <button className='workBtn'>See More</button> */}
    </section>
   )
 }
